a34caa7b3774468814bc6ba7336d47381fc617d7
john.fragkoulis <john.fragkoulis@d3d06036-4249-0410-aff0-259e14133cc1>
2008-03-31 21:28:04 +0000

    made IThread real thread interface for all application threads

--- trunk/thread/IThread.h
+++ trunk/thread/IThread.h
@@ -1,57 +1,36 @@
 #pragma once
-#include <windows.h> // Windows API
 
-namespace tlib
+namespace tlib 
 {
 
 /**
  * @class IThread
- * Windows thread wrapper class.
+ * Portable thread class interface.
+ * This interface is used for deriving platform specific thread classes.
  */
 class IThread
 {
 private:
-    typedef unsigned int (__stdcall *THREADFUNC)(void*);
-
     //! Flag for whether the thread is running
     bool m_bIsRunning;
 
-    //! The thread's os handle
-    HANDLE      m_Handle;
-    
-    //! The thread's parameters
-    LPVOID      m_lpArgs;
-
-    //! Pointer to the main thread function
-    THREADFUNC  m_lpThreadFunc;
-
 public:
     //! Constructor
-    IThread();
+    IThread():m_bIsRunning(false) {}
 
     //! Virtual destructor
-    virtual ~IThread() = 0 {};
+    virtual ~IThread() {}
 
     //! Override this to create and start the thread
-    void Start( void *lpArgs = 0 );
+    virtual void Start( void *lpArgs = 0 ) = 0;
 
     //! Override this to terminate the thread
-    void Terminate();
+    virtual void Terminate() = 0;
 
     //! Returns whether the thread is running
     bool IsRunning() const { return m_bIsRunning; }
 
 protected:
-    static unsigned int __stdcall ThreadFunc( void *lpArgs )
-    {
-        IThread *pParent = reinterpret_cast<IThread*>(lpArgs);
-        pParent->OnStart();
-        pParent->Run( pParent->m_lpArgs );
-        pParent->OnEnd();
-
-        return STILL_ACTIVE;
-    }
-
     //! Sets the running flag
     void Resume() { m_bIsRunning = true; }
 
