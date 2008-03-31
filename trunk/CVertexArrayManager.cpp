0753276a3714729908f480b269d34a1646e419f1
john.fragkoulis <john.fragkoulis@d3d06036-4249-0410-aff0-259e14133cc1>
2008-03-31 21:36:45 +0000

    fixed vertex array manager

--- trunk/CVertexArrayManager.cpp
+++ trunk/CVertexArrayManager.cpp
@@ -1,27 +1,65 @@
-//#include "CVertexArrayManager.h"
-//
-//CVertexArrayManager::~CVertexArrayManager()
-//{
-//    VAList::iterator i = m_Arrays.begin();
-//    for(; i != m_Arrays.end(); ++i )
-//        delete (*i);
-//}
-//
-//void CVertexArrayManager::Enable( const VAKey &key )
-//{
-//    m_Arrays[ key ]->Enable();
-//    
-//    glEnableClientState (GL_TEXTURE_COORD_ARRAY);
-//    glTexCoordPointer   (2, GL_FLOAT, 0, m_TexArray);
-//    glEnableClientState (GL_NORMAL_ARRAY);
-//    glNormalPointer     (GL_FLOAT, 0, m_NormalArray);
-//    glEnableClientState (GL_VERTEX_ARRAY);
-//    glVertexPointer     (3, GL_FLOAT, 0, m_VertexArray);
-//}
-//
-//void CVertexArrayManager::Disable()
-//{
-//    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-//    glDisableClientState(GL_NORMAL_ARRAY);
-//    glDisableClientState(GL_VERTEX_ARRAY);
-//}
+#include "CVertexArrayManager.h"
+//#include "Util/VA/IVertexArray.h"
+#include "Util/VA/CVASphere.h"
+#include "Util/VA/CVAPlane.h"
+#include "Util/std_help.h"
+
+using namespace tlib;
+using namespace tlib::util;
+
+CVertexArrayManager::CVertexArrayManager():
+m_Active(0)
+{}
+
+CVertexArrayManager::~CVertexArrayManager()
+{
+    DELETE_MAP( VAList, m_Arrays );
+}
+
+void CVertexArrayManager::Init()
+{
+    // TODO: Draw this data out of a file
+
+    float radius = 0.05f;
+    int stacks = 5;
+    int slices = 5;
+    CVASphere *sphere = new CVASphere( "BigSphere", radius, stacks, slices );
+    MAP_INSERT( m_Arrays, "BigSphere", sphere );
+
+    radius = 0.02f;
+    sphere = new CVASphere( "SmallSphere", radius, stacks, slices );
+    MAP_INSERT( m_Arrays, "SmallSphere", sphere );
+
+    Vec2f hsize( 0.5f, 0.5f );
+    stacks = slices = 2;
+    CVAPlane *plane = new CVAPlane( "Wall", hsize, stacks, slices );
+    MAP_INSERT( m_Arrays, "Wall", plane );
+
+    hsize.Set( 0.25f, 0.5f );
+    stacks = 20;
+    slices = 30;
+    plane = new CVAPlane( "Cloth", hsize, stacks, slices );
+    MAP_INSERT( m_Arrays, "Cloth", plane );
+}
+
+void CVertexArrayManager::Begin( const VArrayId &key )
+{
+    // If there is another array currently active end it first
+    if( m_Active ) End();
+
+    m_Arrays[ key ]->Begin();
+    m_Active = const_cast<VArrayId*>(&key);
+}
+
+void CVertexArrayManager::Render()
+{
+    assert(m_Active);
+    m_Arrays[ *m_Active ]->Render();
+}
+
+void CVertexArrayManager::End()
+{
+    assert(m_Active);
+    m_Arrays[ *m_Active ]->End();
+    m_Active = 0;
+}
--- trunk/CVertexArrayManager.h
+++ trunk/CVertexArrayManager.h
@@ -2,45 +2,39 @@
 #include "Singleton.h"
 #include <map>
 #include <string>
-using tlib::physics::CCollisionData;
 
 namespace tlib
 {
 
+namespace util
+{
+    class IVertexArray;
+}
+
 class CVertexArrayManager : public Singleton<CVertexArrayManager>
 {
 private:
-    //! Definition of a vertex array
-    struct SVertexArray
-    {
-        //! The object's vertices
-        float (*m_VertexArray)[3];
-    
-        //! The vertices' normals
-        float (*m_NormalArray)[3];
-
-        //! The object's texture coordinates
-        float (*m_TexArray)[2];
-
-        //! The object's index array that defines the polygon
-        unsigned int *m_IndexArray;
-    };
-    
-    typedef unsigned int VAKey;
-    typedef std::map< VAKey, SVertexArray* > VAList;
+    typedef std::string VArrayId;
+    typedef std::map< VArrayId, util::IVertexArray* > VAList;
     
     //! List of vertex arrays
     VAList m_Arrays;
 
+    //! Currently active array
+    VArrayId *m_Active;
+
 public:
-    //! Enables a vertex array
-    void Enable( const VAKey &key );
-    
-    //! Adds a vertex array to the list
-    void Register( 
+    void Init();
+    void Begin( const VArrayId &key );
+    void Render();
+    void End();
+
 private:
     friend Singleton<CVertexArrayManager>;
+    CVertexArrayManager();
     ~CVertexArrayManager();
 };
+
+typedef CVertexArrayManager MGRVertexArray;
 
 } // end namespace tlib
