3dbd11c7d285cbded9f2c4c64dd4fc1906e60ed1
john.fragkoulis <john.fragkoulis@d3d06036-4249-0410-aff0-259e14133cc1>
2008-04-11 16:12:44 +0000

    added cube controls that hold cube rotation data and shared data

---
+++ thread/CubeControls.cpp
@@ -0,0 +1,85 @@
+#include "CubeControls.h"
+using namespace tlib;
+
+//#define WITHOUT_SHARED
+#ifndef WITHOUT_SHARED
+
+bool CubeAngles::Get( float &horizontal, float &vertical ) const
+{
+    if( IsReadable() )
+    {
+        __try { 
+           horizontal = m_fHorizontal;
+           vertical = m_fVertical;
+        }
+        __finally {
+            ReleaseWrite();
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+bool CubeAngles::Set( float horizontal, float vertical )
+{
+    if( IsWritable() )
+    {
+        __try { 
+           m_fHorizontal = horizontal;
+           m_fVertical = vertical;
+        }
+        __finally {
+            ReleaseAll();
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+#else
+
+bool CubeAngles::Get( float &horizontal, float &vertical ) const
+{
+    horizontal = m_fHorizontal;
+    vertical = m_fVertical;
+    return true;
+}
+
+bool CubeAngles::Set( float horizontal, float vertical )
+{
+    m_fHorizontal = horizontal;
+    m_fVertical = vertical;
+    return true;
+}
+
+#endif
+
+CubeControls::CubeControls(): 
+rotationStep(0.025f), 
+horizontalAngle(0.0f), 
+verticalAngle(0.0f)
+{}
+
+void CubeControls::CheckAngles() 
+{
+    // Check cube rotation angles
+    if( horizontalAngle > rotationStep )
+        horizontalAngle -= rotationStep;
+    else if( horizontalAngle <= -rotationStep )
+        horizontalAngle += rotationStep;
+    else
+        horizontalAngle = 0.0f;
+
+    if( verticalAngle > rotationStep )
+        verticalAngle -= rotationStep;
+    else if( verticalAngle <= -rotationStep )
+        verticalAngle += rotationStep;
+    else
+        verticalAngle = 0.0f;
+
+    angles.Set( horizontalAngle, verticalAngle );
+}
