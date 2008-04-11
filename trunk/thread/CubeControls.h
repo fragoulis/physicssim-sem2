f5f860fc8bb814b28499bb66d501c3e907bbc7e5
john.fragkoulis <john.fragkoulis@d3d06036-4249-0410-aff0-259e14133cc1>
2008-04-11 16:12:44 +0000

    added cube controls that hold cube rotation data and shared data

---
+++ trunk/thread/CubeControls.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "ISharedData.h"
+#include "../Math/TQuaternion.h"
+using tlib::math::Quatf;
+
+namespace tlib
+{
+
+/**
+ * Shared memory container class that holds the two cube angles
+ */
+class CubeAngles : public ISharedData
+{
+public:
+    bool Get( float &horizontal, float &vertical ) const;
+    bool Set( float horizontal, float vertical );
+    bool Clear() { return Set( 0.0f, 0.0f ); }
+
+private:
+    float m_fHorizontal;
+    float m_fVertical;
+
+}; // end CubeAngles
+
+struct CubeControls
+{
+public:
+    CubeControls();
+
+    void CheckAngles();
+    void ReadAngles() {
+        angles.Get( horizontalAngle, verticalAngle );
+    }
+
+public:
+    CubeAngles angles;
+    float horizontalAngle, verticalAngle;
+    Quatf rotationAccum;
+
+private:
+    float rotationStep;
+
+}; // end CubeControls
+
+} // end namespace tlib
