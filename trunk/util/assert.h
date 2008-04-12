bc35aeae44fada887b60c8ca751068fe043b428c
john.fragkoulis <john.fragkoulis@d3d06036-4249-0410-aff0-259e14133cc1>
2008-04-12 11:09:44 +0000

    * added fail assert [always fails with a message]

--- trunk/util/assert.h
+++ trunk/util/assert.h
@@ -3,6 +3,8 @@
 
 #ifdef _DEBUG
 #define massert( expression, message ) assert( (expression) && message )
+#define fassert( message )             massert( 0, message )
 #else
 #define massert( expression, message )
+#define fassert( message )
 #endif
